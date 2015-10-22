a086ba2489caa3f7f5b5a19a2e42662e4a2923bd
Harry Wolff <hswolff@gmail.com>
2015-10-22 19:02:43 -0400

    use collectionIds

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var cheerio = require('cheerio');
 
 module.exports = function(Plugin) {
   Plugin.event.file.beforeRender(function(file) {
-    if (file.collectionNames.has('post')) {
+    if (file.collectionIds.has('post')) {
       var $ = cheerio.load(file.data.content);
       var p = $('p').first();
       file.data.excerpt = $.html(p).trim();
