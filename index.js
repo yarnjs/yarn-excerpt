26946a3074e3550abd9843bbc1a1ccecc7c289ed
Harry Wolff <hswolff@gmail.com>
2016-02-05 21:10:35 -0500

    fix for yarn 0.8.0 compatibility

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var cheerio = require('cheerio');
 
 module.exports = function(Plugin) {
-  Plugin.event.file.beforeRender(function(file) {
+  Plugin.event.file.afterRender(function(file, renderedFile) {
     if (file.collectionIds.has('post')) {
-      var $ = cheerio.load(file.data.content);
+      var $ = cheerio.load(renderedFile);
       var p = $('p').first().contents();
       file.data.excerpt = $.html(p).trim();
     }
