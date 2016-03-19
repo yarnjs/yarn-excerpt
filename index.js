366b343018b58cb2e6124ff16fbef2fc1bfc7f3f
Harry Wolff <hswolff@gmail.com>
2016-03-19 18:24:14 -0400

    dont restrict usage to just collections named post

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var cheerio = require('cheerio');
 
 module.exports = function(Plugin) {
   Plugin.event.file.afterRender(function(file, renderedFile) {
-    if (file.collectionIds.has('post')) {
+    if (file && renderedFile) {
       var $ = cheerio.load(renderedFile);
       var p = $('p').first().contents();
       file.data.excerpt = $.html(p).trim();
