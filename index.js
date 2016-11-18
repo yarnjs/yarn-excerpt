2adbf030d6f5d8ed3befdc436a3915a663d1e8be
Alex <AWulkan@users.noreply.github.com>
2016-11-18 20:00:22 +0100

    3 new plugin features
    * Choose between html enhanced or plain text with the "textOnly: <boolean>" option.
    * Limit excerpt length with the "charLimit: <number>" option.
    * Add a custom excerpt with the "customExcerpt: <string>" option.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var cheerio = require('cheerio');
 
-module.exports = function(Plugin) {
+module.exports = function(Plugin, options) {
   Plugin.event.file.afterRender(function(file, renderedFile) {
+
     if (file && renderedFile) {
+      let fd = file.data;
       var $ = cheerio.load(renderedFile);
       var p = $('p').first().contents();
-      file.data.excerpt = $.html(p).trim();
+
+      if (fd[options.customExcerpt]) {
+        fd.excerpt = fd[options.customExcerpt];
+      } else {
+        // Clean text, or html enhanced text
+        options.textOnly ?
+          fd.excerpt = $.text(p).trim():
+          fd.excerpt = $.html(p).trim();
+
+        // Limits the excerpt length to your specified amount
+        if (options.charLimit && fd.excerpt.length > options.charLimit) {
+          fd.excerpt = fd.excerpt.substring(0, options.charLimit - 3) + "...";
+        }
+      }
     }
   });
 };
